c8c8e5a059fafefe7807e357298acd5c5a587f91
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2024-11-19 06:15:12 -0800

    making some changes to mission page

--- packages/nextjs/app/mission/page.tsx
+++ packages/nextjs/app/mission/page.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import CodeSnippet from "./_components/CodeSnippet";
 import GetEnlisted from "./_components/GetEnlisted";
 import type { NextPage } from "next";
 
 const Subgraph: NextPage = () => {
   return (
     <>
-      <GetEnlisted />
+      <div className="flex justify-center top">
+        <h1 className="text-3xl font-bold pt-4 text-center max-w-2xl">Mission 1</h1>
+      </div>
+      <div className="flex justify-center top">
+        <p className="text-lg text-center max-w-2xl">
+          You have arrived at the basecamp, you will need to setup your local development environment so that you can
+          begin your mission.
+        </p>
+      </div>
+      <div className="flex justify-center top mt-4 mb-4">
+        <p>
+          1. Open up a new terminal and run the following command to spin up your local Graph Node inside of docker:
+        </p>
+      </div>
+      <CodeSnippet code="yarn run-node" button={true} />
+      <div className="flex justify-center top mt-4 mb-4">
+        <p>You will know graph-node is ready when you see the following message:</p>
+      </div>
+      <CodeSnippet code="INFO Downloading latest blocks from Ethereum, this may take a few minutes..." button={false} />
     </>
   );
 };
