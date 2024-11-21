cb1d07ce8af98163f0c0ff88023d54300d61fee8
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2024-11-21 12:45:59 -0800

    adding ownable event stuff

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,4 +1,11 @@
-import { CommsEstablished as CommsEstablishedEvent } from "../generated/Comms/Comms";
-import { CommsEstablished } from "../generated/schema";
+import {
+    CommsEstablished as CommsEstablishedEvent,
+    OwnershipTransferred as OwnershipTransferredEvent,
+} from "../generated/Comms/Comms";
+import { CommsEstablished, OwnershipTransferred } from "../generated/schema";
 
 export function handleCommsEstablished(event: CommsEstablishedEvent): void {}
+
+export function handleOwnershipTransferred(
+    event: OwnershipTransferredEvent
+): void {}
