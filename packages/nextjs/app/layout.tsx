ceb40f58d08f44ae1e3894dbde7d70da57cf2d31
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2024-12-03 16:18:27 -0800

    upadte some metadata info

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -4,7 +4,10 @@ import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
-export const metadata = getMetadata({ title: "Scaffold-ETH 2 App", description: "Built with 🏗 Scaffold-ETH 2" });
+export const metadata = getMetadata({
+  title: "Graph Builders Basecamp",
+  description: "A game for learning about The Graph",
+});
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
