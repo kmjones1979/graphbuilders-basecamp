dbd7e19a7c0e581e92ed070c375c045631503bfc
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2024-12-30 11:17:00 -0800

    resize text

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -20,17 +20,17 @@ const Home: NextPage = () => {
           <div className="hero-overlay bg-opacity-20"></div>
           <div className="hero-content text-neutral-content flex flex-col items-center justify-center w-full h-screen px-4 sm:px-8">
             <div className="max-w-1xl text-center">
-              <h2 className="mb-4 font-bold text-1xl text-white">Welcome to</h2>
-              <h1 className="mb-4 text-3xl sm:text-5xl text-white welcome-text">The Graph</h1>
-              <h1 className="mb-4 text-3xl sm:text-5xl text-white welcome-text">Builders Basecamp</h1>
+              <h2 className="mb-7 font-bold text-2xl text-white">Welcome to</h2>
+              <h1 className="mb-4 text-4xl sm:text-6xl text-white welcome-text">The Graph</h1>
+              <h1 className="mb-3 text-4xl sm:text-6xl text-white welcome-text">Builders Basecamp</h1>
               <div className="flex flex-col items-center">
-                <p className="mb-5 max-w-lg text-center text-white text-sm sm:text-base">
+                <p className="mb-10 max-w-lg text-center text-white text-sm sm:text-xl">
                   A series of missions where you will test your ability to write smart contracts and develop subgraphs
                   on The Graph protocol. To get started click the button below.
                 </p>
                 <div className="flex items-center justify-center">
                   <button
-                    className="btn btn-success text-sm sm:text-base py-2 px-4"
+                    className="btn btn-primary text-lg sm:text-base py-2 px-4"
                     onClick={() => router.push("/mission-0")}
                   >
                     Begin
